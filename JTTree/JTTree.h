59a0163f8dea971ad300abd2f68638b69fd6f3f6
Valentin Radu <radu.v.valentin@gmail.com>
2017-05-04 22:29:37 +0300

    Fixed header issues

--- JTTree/JTTree.h
+++ JTTree/JTTree.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char JTTreeVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <JTTree/PublicHeader.h>
 
 
+#import <JTTree/JTTree.h>
