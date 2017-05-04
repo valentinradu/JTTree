e8a6a28708c4f889a25f24cbca73c1693bad8047
Valentin Radu <radu.v.valentin@gmail.com>
2017-05-04 22:07:24 +0300

    Added Cocoa Touch framework

---
+++ JTTree/JTTree.h
@@ -0,0 +1,19 @@
+//
+//  JTTree.h
+//  JTTree
+//
+//  Created by Valentin Radu on 04/05/2017.
+//  Copyright © 2017 Jacob Bandes-Storch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTTree.
+FOUNDATION_EXPORT double JTTreeVersionNumber;
+
+//! Project version string for JTTree.
+FOUNDATION_EXPORT const unsigned char JTTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTTree/PublicHeader.h>
+
+
